48682d03ddc8a1a5609ab3e2bf897e6ab112b4b2
Janine Erika Marie M. Balangue <janine@smartworksintl.com>
2023-06-05 10:25:48 +0800

    Initial Files Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
